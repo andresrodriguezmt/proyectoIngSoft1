4be971d8f222ff2abe58d21a6c615e9a91d484fb
andresrodriguezmt <gonzaloandres222@hotmail.com>
2024-04-16 19:03:14 -0500

    Commit: Se actualiza el repositorio para limpiarlo y actualizarlo

---
+++ src/test/java/co/ucentral/sistemas/proyectoCitasG15/ProyectoCitasG15ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.sistemas.proyectoCitasG15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCitasG15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
