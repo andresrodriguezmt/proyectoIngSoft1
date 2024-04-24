817c3558f2c9768b82a703a438bc2bde9ca52702
andresrodriguezmt <gonzaloandres222@hotmail.com>
2024-04-24 18:16:12 -0500

    master: Limpiado del proyecto

--- src/test/java/co/ucentral/sistemas/proyectoCitasG15/ProyectoCitasG15ApplicationTests.java
+++ src/test/java/co/ucentral/sistemas/proyectoCitasG15/ProyectoCitasG15ApplicationTests.java
@@ -1,10 +1,10 @@
-package co.vinni.appDemo;
+package co.ucentral.sistemas.proyectoCitasG15;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AppDemoApplicationTests {
+class ProyectoCitasG15ApplicationTests {
 
 	@Test
 	void contextLoads() {
